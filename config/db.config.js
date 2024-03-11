a5625cac26d970a8501633633106b55aec612b1a
soufiane570 <soufiane.hamdach28@gmail.com>
2024-03-11 13:59:45 +0000

    update de hostname de database 2

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,18 @@
-module.exports = {
-    host: "41.142.38.218",
-    user: "root",
-    password: "",
-    database: "pfe"
-  };
+// module.exports = {
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "pfe"
+//   };
 // module.exports = {
 //   host: "mysql2.serv00.com",
 //   user: "m5665_otvmovie",
 //   password: "dh-e8.=e+.,as.8P7z29H0Em.Y1hl_",
 //   database: "m5665_PFE"
 // };
+module.exports = {
+  host: "bhbwxlf3u1m5h6kcmlc9-mysql.services.clever-cloud.com",
+  user: "unhv3iec8xpxxmux",
+  password: "VC98I4ojNkvbDgAvAj2I",
+  database: "bhbwxlf3u1m5h6kcmlc9"
+};
